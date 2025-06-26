b0931265dcf8cfe93bcc2957c4f219b67215e8c2
Stupel <marian.holly7117@gmail.com>
2025-06-26 23:09:44 +0200

    database

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "api.User"
