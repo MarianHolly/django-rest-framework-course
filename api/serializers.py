ff2b603fd4a9b57e643e0c6837347d297ed20e52
Stupel <marian.holly7117@gmail.com>
2025-06-28 23:30:05 +0200

    SerializerMethodField

--- api/serializers.py
+++ api/serializers.py
@@ -22,7 +22,12 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_items.item_subtotal for order_items in order_items)
 
     class Meta:
         model = Order
-        fields = ('order_id', 'created_at', 'user', 'status', 'items')
+        fields = ('order_id', 'created_at', 'user', 'status', 'items', 'total_price')
