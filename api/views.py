a2c6a6bbf610283d42a0e86b1481ce175c50c0a6
Stupel <marian.holly7117@gmail.com>
2025-06-28 23:57:40 +0200

    aggregate API data

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from django.db.models.aggregates import Max
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404
 
 from api.models import Product, Order
-from api.serializers import ProductSerializer, OrderSerializer
+from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -25,4 +26,15 @@ def product_detail(request, pk):
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']
+    })
     return Response(serializer.data)
